15252151e627fa2d18b9558b72ca23fc654179ea
hamzamix <51834761+hamzamix@users.noreply.github.com>
2025-07-24 11:23:34 -0500

    Create index.tsx

---
+++ frontend/src/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { ThemeProvider } from './hooks/useTheme';
+import './index.css';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider storageKey="starwise-ui-theme">
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
