634eb93e2704a60ad0e02af8af083e522f0e3567
hamza mribti <51834761+hamzamix@users.noreply.github.com>
2025-08-26 01:11:53 +0100

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -31,7 +31,8 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 
-const API_BASE_URL = 'http://localhost:4000';
+// Dynamic API URL that adapts to the current host
+const API_BASE_URL = `${window.location.protocol}//${window.location.hostname}:4000`;
 axios.defaults.withCredentials = true;
 
 
